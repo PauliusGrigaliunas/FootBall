1ed858a21d16c07d050e4928d9aa743cb2348517
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2017-12-09 18:50:07 +0200

    +statistics

---
+++ WebFootball/WebFootball/Controllers/StatisticController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebFootball.Controllers
+{
+    public class StatisticController : Controller
+    {
+        // GET: Statistic
+        public ActionResult Statistic()
+        {
+            return View();
+        }
+    }
+}
